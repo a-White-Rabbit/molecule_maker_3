e47fe7545986dffe2ad3be2739400fd1591159e7
Marc E Auciello <you@example.com>
2022-04-10 00:13:34 -0400

    +users controller, basic function

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+   def new
+      if !logged_in?
+         @farmer = Farmer.new
+      else
+         redirect_to home_path
+      end
+   end
+
+   def create
+      farmer = Farmer.new(farmer_params)
+      if farmer.save
+         session[:farmer_id] = farmer.id
+         redirect_to home_path   
+      else
+         render :new
+      end
+   end
+
+
+
+   private
+
+   def farmer_params
+      params.require(:farmer).permit(:username, :email, :password)
+   end
+
+end
