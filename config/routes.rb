ca7d122f75450dff9237ac1fd5a74b6f36c58408
Marc E Auciello <you@example.com>
2022-04-09 23:37:46 -0400

    +pages routes, navbar, stylesheet

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  get 'pages/about'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root    'pages#index', as: 'home'
+
+  # PAGES
+  get     'about' =>  'pages#about'
+
+  # COMMENTS / Rails gen routes
+  # get 'pages/index'
+  # get 'pages/about'
+
 end
