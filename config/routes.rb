612a34cef6640d9a7392b48dc9014b27135b6fd4
Marc E Auciello <you@example.com>
2022-04-10 00:10:01 -0400

    +user, +sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,25 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
 
   root    'pages#index', as: 'home'
 
   # PAGES
   get     'about' =>  'pages#about'
 
+  # USERS
+  get     '/signup', to: 'users#new'
+  post    '/signup', to: 'users#create'
+  resources :users
+
+  # SESSIONS
+  get     '/login', to: 'sessions#new'
+  post    '/login', to: 'sessions#create'
+  get     '/logout', to: 'sessions#destroy'
+
   # COMMENTS / Rails gen routes
   # get 'pages/index'
   # get 'pages/about'
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
 
 end
