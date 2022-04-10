142f21c075657e9ab84c6a006672dbd39a83207e
Marc E Auciello <you@example.com>
2022-04-10 00:27:38 -0400

    sessions controller main functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,24 @@
 class SessionsController < ApplicationController
+  
   def new
+    if logged_in?
+      redirect_to home_path      
+    end
   end
 
   def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to home_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    session.delete :user_id
+    redirect_to login_path
   end
+
 end
