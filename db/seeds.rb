c9d41bad127974e2f4d34d2593b0ba7a82f24823
Marc E Auciello <you@example.com>
2022-04-10 00:39:09 -0400

    +user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# USERS
+User.destroy_all
+user1 = User.create({
+   :name=>"Wayne",
+   :email=>"wayne@wayne.com",
+   :username=>"wayne",
+   :password=>"wayne",
+   })
+user2 = User.create({
+   :name=>"Daryl",
+   :email=>"daryl@daryl.com",
+   :username=>"daryl",
+   :password=>"daryl",
+   })
+user3 = User.create({
+   :name=>"Dan",
+   :email=>"dan@dan.com",
+   :username=>"dan",
+   :password=>"dan",
+   })
+
+puts "Total number of users: #{User.all.count}"
+puts "User names: #{User.all.pluck("name")}"
+puts "User1: #{user1.name} username: #{user1.username} password: #{user1.password}"
+puts "User2: #{user2.name} username: #{user2.username} password: #{user2.password}"
+puts "User3: #{user3.name} username: #{user3.username} password: #{user3.password}"
